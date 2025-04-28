69deab60f7aeb7178bdc3a82de4d9eba687b9a08
inesuribeb <inesuribeb@gmail.com>
2025-04-28 12:07:04 +0200

    efecto superposicion de la seccion content-container en LandingPage

--- mi-aplicacion/src/pages/LandingPage/Landing.jsx
+++ mi-aplicacion/src/pages/LandingPage/Landing.jsx
@@ -8,14 +8,44 @@ import './Landing.css'
 function Landing() {
 
     return (
-        <>           
-            <Welcome />
-            <InmoLocation />
-            <CarouselLocation />
-            <BuyHomes />
-            <PropertiesLand />
-        </>
+        <div className="main-landing-container"> 
+            <section className="welcome-section">         
+                <Welcome />
+            </section>
+            <section className="content-container">
+                <InmoLocation />
+                <CarouselLocation />
+                <BuyHomes />
+                <PropertiesLand />
+            </section>
+        </div>
     );
 }
 
 export default Landing;
+
+// import Welcome from "./components/Welcome";
+// import InmoLocation from "./components/InmoLocation";
+// import CarouselLocation from "./components/CarouselLocation";
+// import PropertiesLand from "./components/PropertiesLand";
+// import BuyHomes from "./components/BuyHomes";
+// import './Landing.css'
+
+// function Landing() {
+//     return (
+//         <div className="landing-container">
+//             <section className="welcome-section">
+//                 <Welcome />
+//             </section>
+            
+//             <section className="content-container">
+//                 <InmoLocation />
+//                 <CarouselLocation />
+//                 <BuyHomes />
+//                 <PropertiesLand />
+//             </section>
+//         </div>
+//     );
+// }
+
+// export default Landing;
